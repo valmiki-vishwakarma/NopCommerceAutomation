93905a4c269114c704dff0547ff2cc17fdb62b84
Valmiki Vishwakarma <you@example.com>
2022-12-31 21:04:52 +0530

    Commit files.

--- src/test/java/com/nopcommerce/pageObjects/AddcustomerPage.java
+++ src/test/java/com/nopcommerce/pageObjects/AddcustomerPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+//Test
 public class AddcustomerPage {
 
 	public WebDriver driver;
